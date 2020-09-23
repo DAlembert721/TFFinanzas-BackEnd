f5a4df1650f8ad9d91a098bab671d46ababa766e
Dalembert Monzon <DDMU@users.noreply.github.com>
2020-09-23 11:38:45 -0500

    Initial Commit
    Creacion de proyecto

---
+++ src/main/java/com/acme/otorongo/OtorongoApplication.java
@@ -0,0 +1,13 @@
+package com.acme.otorongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtorongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtorongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/otorongo/OtorongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.otorongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtorongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
